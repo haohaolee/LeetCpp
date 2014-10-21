40a301177538300f56ea54e4ce0e5d0ccfa928af
Su <sushi@microsoft.com>
2014-10-21 13:59:27 +0800

    update the solutions.

---
+++ symmetric_tree.cpp
@@ -0,0 +1,20 @@
+/*
+ * Symmetric Tree .
+ * https://oj.leetcode.com/problems/symmetric-tree/
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+
+    }
+};
