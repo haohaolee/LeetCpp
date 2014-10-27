5e8197f0b37c542942f89f5f4da7419a2e9be4ea
Su <sushi@microsoft.com>
2014-10-28 00:11:43 +0800

    code update for word search.

--- word_search.cpp
+++ word_search.cpp
@@ -11,8 +11,8 @@ using namespace std;
 class Solution {
 private:
   typedef vector<vector<char>> word_board;
-  size_t board_row_size;
-  size_t board_col_size;
+  int board_row_size;
+  int board_col_size;
 
   // Left, right, down, up.
   int dirs[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
@@ -26,7 +26,7 @@ private:
     board_col_size = board[0].size();
     vector<bool> row(board_col_size, false);
 
-    for (size_t i = 0; i < board_row_size; ++i) {
+    for (int i = 0; i < board_row_size; ++i) {
       if (board[i].size() != board_col_size) {
         throw "Current row is not the same size as previous one!";
       }
@@ -36,13 +36,14 @@ private:
   }
 
   bool back_track(const word_board &board,
-                  size_t x, size_t y,
-                  const string& word, size_t matching_pos) {
+                  int x, int y,
+                  const string& word, int matching_pos) {
     // Edge case which means we matched before.
     if (matching_pos >= word.length())
       return true;
 
-    if (x >= board_col_size || y >= board_row_size ||
+    if (x < 0 || x >= board_col_size ||
+        y < 0 || y >= board_row_size ||
         visited[y][x])
       return false;
 
@@ -53,7 +54,7 @@ private:
     visited[y][x] = true;
     bool matching = false;
 
-    for (size_t i = 0; i < 4 && !matching; ++i) {
+    for (int i = 0; i < 4 && !matching; ++i) {
       matching = back_track(board, x + dirs[i][0], y + dirs[i][1],
                             word, matching_pos + 1);
     }
@@ -74,9 +75,9 @@ public:
 
       bool matching = false;
 
-      for (size_t i = 0; i < board_row_size && !matching; ++i) {
-        for (size_t j = 0; j < board_col_size && !matching; ++j) {
-          matching = back_track(board, i, j, word, 0);
+      for (int y = 0; y < board_row_size && !matching; ++y) {
+        for (int x = 0; x < board_col_size && !matching; ++x) {
+          matching = back_track(board, x, y, word, 0);
         }
       }
 
