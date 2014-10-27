078a44208d9f88548ce0efe37084368158df309e
Su <sushi@microsoft.com>
2014-10-27 23:40:29 +0800

    code update for word search.

--- word_search.cpp
+++ word_search.cpp
@@ -10,18 +10,24 @@ using namespace std;
 
 class Solution {
 private:
-  typedef vector<vector<int>> word_board;
+  typedef vector<vector<char>> word_board;
   size_t board_row_size;
   size_t board_col_size;
-  int dirs[2][2] = {};
+
+  // Left, right, down, up.
+  int dirs[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
   vector<vector<bool>> visited;
   void init(const word_board& board) {
-    board_row_size = board[0].size();
-    vector<bool> row(row_size, false);
+    if (board.size() == 0 || board[0].size() == 0)
+      throw "Board is empty!";
 
-    for (size_t i = 0; i < board.size(); ++i) {
-      if (board[i].size() != row_size) {
+    board_row_size = board.size();
+    board_col_size = board[0].size();
+    vector<bool> row(board_col_size, false);
+
+    for (size_t i = 0; i < board_row_size; ++i) {
+      if (board[i].size() != board_col_size) {
         throw "Current row is not the same size as previous one!";
       }
 
@@ -29,14 +35,16 @@ private:
     }
   }
 
-  bool back_track(const vector<vector<char>> &board,
-                 size_t x, size_t y,
-                 const string& word, size_t matching_pos) {
+  bool back_track(const word_board &board,
+                  size_t x, size_t y,
+                  const string& word, size_t matching_pos) {
     // Edge case which means we matched before.
-    if (x >= board[0].size() || y >= board.size() ||
-        matching_pos >= word.length()) {
-          return true;
-    }
+    if (matching_pos >= word.length())
+      return true;
+
+    if (x >= board_col_size || y >= board_row_size ||
+        visited[y][x])
+      return false;
 
     if (word[matching_pos] != board[y][x])
       return false;
@@ -45,24 +53,9 @@ private:
     visited[y][x] = true;
     bool matching = false;
 
-    // UP
-    if (!matching && y >= 1) {
-      matching ||= back_track(board, x, y - 1, word, matching_pos + 1);
-    }
-
-    // Down
-    if (!matching && (y + 1) < board.size()) {
-      matching ||= back_track(board, x, y + 1, word, matching_pos + 1);
-    }
-
-    // Left
-    if (!matching && x >= 1) {
-      matching ||= back_track(board, x - 1, y, word, matching_pos + 1);
-    }
-
-    // Right
-    if (!matching && (x + 1) < board[0].size()) {
-      matching ||= back_track(board, x, y + 1, word, matching_pos + 1);
+    for (size_t i = 0; i < 4 && !matching; ++i) {
+      matching = back_track(board, x + dirs[i][0], y + dirs[i][1],
+                            word, matching_pos + 1);
     }
 
     // Clear mask
@@ -72,20 +65,18 @@ private:
   }
 
 public:
-    bool exist(vector<vector<char>> &board, string word) {
-      if (board.size() == 0 || board[0].size() == 0 || word.length() == 0)
-        return false;
-
+    bool exist(word_board &board, string word) {
       // Init the visited matrix for board before using backtracking.
       init(board);
 
-      size_t row_size = board.size();
-      size_t col_size = board[0].size();
+      if (word.length() == 0)
+        return false;
+
       bool matching = false;
 
-      for (size_t i = 0; i < row_size && !matching; ++i) {
-        for (size_t j = 0; j < col_size && !matching; ++j) {
-          matching = backTrack(board, i, j, word, 0);
+      for (size_t i = 0; i < board_row_size && !matching; ++i) {
+        for (size_t j = 0; j < board_col_size && !matching; ++j) {
+          matching = back_track(board, i, j, word, 0);
         }
       }
 
