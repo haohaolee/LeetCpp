c4e1eb09a7eaf72f277a27007bf2ccd8ed2d529f
Su <sushi@microsoft.com>
2014-10-28 22:04:44 +0800

    add basic test case for word search problem.

--- test/word_search_unittest.cc
+++ test/word_search_unittest.cc
@@ -1,3 +1,7 @@
+#include <vector>
+#include <string>
+using namespace std;
+
 #include <gtest\gtest.h>
 
 #include "..\solutions\word_search.h"
@@ -5,5 +9,17 @@
 namespace {
   TEST(WordSearchTest, Exist) {
     WordSearch solution;
+
+    vector<vector<char>> board;
+    board.push_back(vector<char>({'A', 'B', 'C', 'E' }));
+    board.push_back(vector<char>({ 'S', 'F', 'C', 'S' }));
+    board.push_back(vector<char>({ 'A', 'D', 'E', 'E' }));
+    string word1 = "ABCCED";
+    string word2 = "SEE";
+    string word3 = "ABCB";
+
+    EXPECT_EQ(true, solution.exist(board, word1));
+    EXPECT_EQ(true, solution.exist(board, word2));
+    EXPECT_EQ(false, solution.exist(board, word3));
   }
 }
