87c8277ea99d57bfc74c397ab6535a2b6117c8c1
metacpp <carmack.shi@gmail.com>
2014-11-03 22:55:00 +0800

    add folder for doc.

--- solutions/remove_duplicates_from_sorted_array.h
+++ solutions/remove_duplicates_from_sorted_array.h
@@ -1,7 +1,7 @@
-/*
+/*******************************************************************************
 * Remove Duplicates from Sorted Array.
 * https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/
-*/
+*******************************************************************************/
 
 #ifndef LEETCPP_SOLUTIONS_REMOVE_DUPLICATES_FROM_SORTED_ARRAY_H_
 #define LEETCPP_SOLUTIONS_REMOVE_DUPLICATES_FROM_SORTED_ARRAY_H_
