3097fc399f0e9b6579d997c06c32b4da487ad681
metacpp <carmack.shi@gmail.com>
2014-11-07 08:52:56 +0800

    solution for plus one.

--- solutions/plus_one.cc
+++ solutions/plus_one.cc
@@ -3,6 +3,21 @@ using namespace std;
 
 #include "plus_one.h"
 
-vector<int> PlusOne::plusOne(std::vector<int> &digits) {
-  return vector<int>();
+vector<int> PlusOne::plusOne(vector<int> &digits) {
+  if (digits.empty())
+    return digits;
+
+  int carry = 1;
+
+  for (int i = digits.size() - 1; i >= 0 && carry > 0; --i) {
+    digits[i] += carry;
+    carry = digits[i] / 10;
+    digits[i] %= 10;
+  }
+
+  if (carry > 0) {
+    digits.insert(digits.begin(), carry);
+  }
+
+  return digits;
 }
--- solutions/plus_one_unittest.cc
+++ solutions/plus_one_unittest.cc
@@ -9,8 +9,20 @@ namespace {
   TEST(PlusOneTest, Case1) {
     PlusOne solution;
     vector<int> digits = { 1, 1, 2 };
-    vector<int> result = { 1, 1, 3 };
+    vector<int> expectedResult = { 1, 1, 3 };
+    vector<int> result = solution.plusOne(digits);
 
-    //EXPECT_EQ(result, solution.plusOne(digits));
+    EXPECT_EQ(expectedResult.size(), result.size());
+    EXPECT_TRUE(equal(expectedResult.rbegin(), expectedResult.rend(), result.rbegin()));
+  };
+
+  TEST(PlusOneTest, Case2) {
+    PlusOne solution;
+    vector<int> digits = { 9, 9 };
+    vector<int> expectedResult = { 1, 0, 0 };
+    vector<int> result = solution.plusOne(digits);
+
+    EXPECT_EQ(expectedResult.size(), result.size());
+    EXPECT_TRUE(equal(expectedResult.rbegin(), expectedResult.rend(), result.rbegin()));
   };
 }
