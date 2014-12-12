59abd6441adac6a09c5a81b8aca4b56726d06555
metacpp <carmack.shi@gmail.com>
2014-12-12 21:14:20 +0800

    update recursive solution for binary tree inorder traversal.

--- solutions/tree/binary_tree_inorder_traversal.cc
+++ solutions/tree/binary_tree_inorder_traversal.cc
@@ -6,5 +6,21 @@ using namespace std;
 #include "../util/util.h"
 
 vector<int> BinaryTreeInorderTraversal::inorderTraversal(TreeNode *root) {
-  return vector<int>();
+  vector<int> traversal_history;
+
+  if (root == NULL)
+    return traversal_history;
+
+  // Traverse the left child sub-tree.
+  vector<int> left_traversal_history = inorderTraversal(root->left);
+  traversal_history.insert(traversal_history.end(),
+                           left_traversal_history.begin(),
+                           left_traversal_history.end());
+  traversal_history.push_back(root->val);
+  vector<int> right_traversal_history = inorderTraversal(root->right);
+  traversal_history.insert(traversal_history.end(),
+                           right_traversal_history.begin(),
+                           right_traversal_history.end());
+
+  return traversal_history;
 }
--- solutions/tree/binary_tree_inorder_traversal_unittest.cc
+++ solutions/tree/binary_tree_inorder_traversal_unittest.cc
@@ -3,32 +3,34 @@ using namespace std;
 
 #include <gtest/gtest.h>
 
-#include "path_sum.h"
+#include "binary_tree_inorder_traversal.h"
 #include "../util/util.h"
 
 namespace {
-  TEST(PathSumTest, Case1) {
-    PathSum solution;
+  TEST(BinaryTreeInorderTraversalTest, Case1) {
+    BinaryTreeInorderTraversal solution;
     TreeNode* root = NULL;
 
     // Empty tree.
-    EXPECT_FALSE(solution.hasPathSum(NULL, 0));
+    EXPECT_TRUE(solution.inorderTraversal(NULL).empty());
     root = build_tree("#");
-    EXPECT_FALSE(solution.hasPathSum(root, 0));
+    EXPECT_TRUE(solution.inorderTraversal(root).empty());
     destroy_tree(root);
 
     // Single root node tree.
+    vector<int> expected_result = { 1 };
     root = build_tree("1");
-    EXPECT_TRUE(solution.hasPathSum(root, 1));
+    EXPECT_EQ(expected_result, solution.inorderTraversal(root));
     destroy_tree(root);
   };
 
-  TEST(PathSumTest, Case2) {
-    PathSum solution;
+  TEST(BinaryTreeInorderTraversalTest, Case2) {
+    BinaryTreeInorderTraversal solution;
     TreeNode* root = NULL;
 
     root = build_tree("1,2,2,3,#,#,3,4,#,#,4");
-    EXPECT_TRUE(solution.hasPathSum(root, 10));
+    vector<int> expected_result = { 4, 3, 2, 1, 2, 3, 4 };
+    EXPECT_EQ(expected_result, solution.inorderTraversal(root));
     destroy_tree(root);
   };
 }
