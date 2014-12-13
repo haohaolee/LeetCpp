0a5360013ed4d5958657855838c20df51c364ecf
metacpp <carmack.shi@gmail.com>
2014-12-13 23:51:26 +0800

    update the ut for util.

--- solutions/util/util_unittest.cc
+++ solutions/util/util_unittest.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 using namespace std;
 
@@ -16,14 +17,13 @@ namespace {
     destroy_tree(root);
   };
 
-  // TODO: support the trim the token.
-  //TEST(UtilTest, Case2) {
-  //  string tree_data = "5, 4, 8, 11, #, 13, 4, 7, 2, #, #, 5, 1";
-  //  TreeNode* root = NULL;
+  TEST(UtilTest, Case2) {
+    string tree_data = "5,4,8,11,#,13,4,7,2,#,#,5,1";
+    TreeNode* root = NULL;
 
-  //  root = build_tree(tree_data);
-  //  string output = output_tree(root);
-  //  EXPECT_EQ(tree_data, output);
-  //  destroy_tree(root);
-  //};
+    root = build_tree(tree_data);
+    string output = output_tree(root);
+    EXPECT_EQ(tree_data, output);
+    destroy_tree(root);
+  };
 }
